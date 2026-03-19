81e87d518af1e72c20d5301cbe912cd5adfb9566
satriolenggo <satriolenggo@gmail.com>
2026-03-19 15:39:50 +0700

    Add files via upload

---
+++ webcam nata bata/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__,
+            template_folder='templates',
+            static_folder='static',
+            static_url_path='/static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('index.html'), 200
+
+if __name__ == '__main__':
+    app.run(debug=False)
